0252922fcbd6f2557c701cc01b5b2418ed0b66f2
Barak Rosner <rosnerbb@gmail.com>
2021-02-14 03:31:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
-    if lowest_value == nil v < lowest_value 
+    if lowest_value == nil || v < lowest_value 
       lowest_value = v
       lowest_key = k 
     end
